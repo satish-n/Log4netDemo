222b9637c90e75435ecc07d09935495abf6c1163
Satish N <satishnathe@gmail.com>
2017-05-04 17:25:17 +0530

    Add files via upload

---
+++ Log4netDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Log4netDemo
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            log.Debug("Application started");
+            try
+            {
+                log.Debug("Debug logging");
+                log.Info("Info logging");
+                log.Warn("Warn logging");
+                log.Error("Error logging");
+                log.Fatal("Fatal logging");
+
+
+                double pi = 3.14159, zero = 0.0;
+                double result = pi / zero;
+            }
+            catch (Exception ex)
+            {
+
+                log.Debug("Debug error logging", ex);
+                log.Info("Info error logging", ex);
+                log.Warn("Warn error logging", ex);
+                log.Error("Error error logging", ex);
+                log.Fatal("Fatal error logging", ex);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Log4netDemo
-Log4net Demo with the latest version dll 2.0.8.0 
+Log4net Demo with the latest version of the dll 2.0.8.0
+
+# Documentation
+
+For the latest documentation see the log4net web site at:
+http://logging.apache.org/log4net
